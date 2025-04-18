6e674518378b60e0ed337009f602edfc4b8b3566
Levar Norwood <46905650+lev2pr0@users.noreply.github.com>
2025-04-17 21:08:33 -0400

    ready for testing

--- dlmailboxfwdreport.ps1
+++ dlmailboxfwdreport.ps1
@@ -11,22 +11,22 @@ Function main{
 
     # Connect to Exchange Online and skips if -onpremEX switch is found
     if (-not $onpremEX) {
-        Write-Host "Connecting to Exchange Online..." -ForegroundColor Cyan
+        Write-Host "Connecting to Exchange Online...`n" -ForegroundColor Cyan
         try { # Check for existing Exchange Online sessions
         $exchSessions = (Get-ConnectionInformation | Where-Object {$_.name -like "*ExchangeOnline*"})
         if ($exchSessions.count -lt 1) { # Connect to Exchange Online if no existing session
             Connect-ExchangeOnline
         } else { # Confirm existing connection to Exchange Online
-            Write-Host "Already connected to Exchange Online." -ForegroundColor Green
+            Write-Host "Already connected to Exchange Online.`n" -ForegroundColor Green
         }
     } catch { # Handle errors connecting to Exchange Online
-        Write-Host "Error connecting to Exchange Online: $_" -ForegroundColor Red
-        Write-Host "If using Exchange Management Shell on Exchange Server, then rerun using -onpremEX switch" -ForegroundColor Red
-        Write-Host "Exiting script." -ForegroundColor Red
+        Write-Host "Error connecting to Exchange Online: $_`n" -ForegroundColor Red
+        Write-Host "If using Exchange Management Shell on Exchange Server, then rerun using -onpremEX switch `n" -ForegroundColor Red
+        Write-Host "Existing script. Goodbye.`n" -ForegroundColor Red
         return
             }
     } else { # Skip Exchange Online session check and connection
-        Write-Host "Skipping Exchange Online connection as -onpremEX is provided." -ForegroundColor Cyan
+        Write-Host "Skipping Exchange Online connection as -onpremEX is provided.`n" -ForegroundColor Cyan
     }
 
     # Gather domains to consider internal for report
@@ -37,12 +37,43 @@ Function main{
     $Domains = $Domains | Where-Object { $_ -match '^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$' }
     if ($Domains.count -lt 1) { # Handle errors for invalid user inputs
         Write-Host "No valid domains provided." -ForegroundColor Red
-        Write-Host "Please provide a valid domain list in the format: domain1.com,domain2.com" -ForegroundColor Red
-        Write-Host "Exiting script." -ForegroundColor Red
+        Write-Host "Please provide a valid domain list in the format: domain1.com,domain2.com`n" -ForegroundColor Red
+        Write-Host "Existing script. Goodbye.`n" -ForegroundColor Red
         return
         }
     }
 
+    # Reporting function start
+    Write-Host "Ready to run your report `n" -ForegroundColor Green
+    # Prompt user for report type
+    do {
+        Write-Host "Available reports: `n" -ForegroundColor Yellow
+        Write-Host "1. publicDL - Public Distribution List Report" -ForegroundColor Yellow
+        Write-Host "2. mailboxfwd - Mailbox Forwarding Report" -ForegroundColor Yellow
+        Write-Host "3. both - Run both reports `n" -ForegroundColor Yellow
+        Write-Host "Type 'exit' to quit the script. `n" -ForegroundColor Yellow
+        $reportType = Read-Host "Enter the report type you want to run (publicDL, mailboxfwd, both)"
+        
+        if ([string]::IsNullOrEmpty($reportType)) {
+            Write-Host "Input cannot be empty. Please enter a valid report type." -ForegroundColor Red
+        } elseif ($reportType -notin @("publicDL", "mailboxfwd", "both", "exit")) {
+            Write-Host "Invalid report type. Please enter 'publicDL', 'mailboxfwd', or 'both'." -ForegroundColor Red
+        }
+    } while ([string]::IsNullOrEmpty($reportType) -or $reportType -notin @("publicDL", "mailboxfwd", "both", "exit"))
+
+    # Run functions based on user input
+    if ($reportType -eq "publicDL") {
+        publicDL_report
+    } elseif ($reportType -eq "mailboxfwd") {
+        mailboxfwd_report
+    } elseif ($reportType -eq "both") {
+        Write-Host "Running both reports... `n" -ForegroundColor Green
+        publicDL_report
+        mailboxfwd_report
+    } elseif ($reportType -eq "exit") {
+        Write-Host "Existing script. Thanks. Goodbye.`n" -ForegroundColor Red
+        break
+    }
 }
 
 # Export results to CSV for report function
@@ -52,6 +83,7 @@ Function report_csv {
     )
     if ($results.Count -gt 0) {
         try {
+            Write-Host "Exporting results to CSV...`n" -ForegroundColor Cyan
             $results | Export-Csv -Path $OutputPath -NoTypeInformation
             Write-Host "Report exported to $OutputPath" -ForegroundColor Green
         } catch {
@@ -66,16 +98,17 @@ Function report_csv {
 Function publicDL_report {
     # Get all distribution groups that are public
     try {
+        Write-Host "Running Public Distribution Report... `n" -ForegroundColor Cyan
         $public_groups = Get-DistributionGroup | Where-Object {$_.RequireSenderAuthenticationEnabled -eq $false}
     } catch {
-        Write-Host "Error retrieving public distribution group: $($_.PrimarySmtpAddress): $_" -ForegroundColor Red
+        Write-Host "Error retrieving public distribution group: $($_.PrimarySmtpAddress): $_ `n" -ForegroundColor Red
         return
     }
 
     # Get members of each public distribution group
     $results = @()
     $public_groups | ForEach-Object {
-        Write-host "Processing members of $($_.PrimarySmtpAddress)" -ForegroundColor Cyan
+        Write-host "Processing members of $($_.PrimarySmtpAddress) `n" -ForegroundColor Cyan
         $members = Get-DistributionGroupMember -Identity $_.name
         foreach ($member in $members) {
              # Get recipient details for each member
@@ -89,7 +122,7 @@ Function publicDL_report {
                         GroupType = $_.RecipientTypeDetails
                     }
                 } else {
-                    Write-Host "Recipient not found for member $($member.name)" -ForegroundColor Yellow
+                    Write-Host "Recipient not found for member $($member.name) `n" -ForegroundColor Yellow
                 }
             }
         }
@@ -99,17 +132,16 @@ Function publicDL_report {
     # Display results in console  
 }
 
-
 #Mailbox Forward Report Function to pull forwarding report for User and Shared Mailbox
 Function mailboxfwd_report {
     # Get all user and shared mailboxes with forwardingSMTPaddress
-    Write-Host "Gathering mailboxes with forwarding addresses..." -ForegroundColor Cyan
+    Write-Host "Running Mailbox Forwarding Report...`n" -ForegroundColor Cyan
     try { 
     $mailboxes = Get-Mailbox -ResultSize Unlimited | Where-Object {
         $_.RecipientTypeDetails -eq "UserMailbox" -or $_.RecipientTypeDetails -eq "SharedMailbox"
         } | Where-Object {$_.ForwardingSmtpAddress -ne $null}
     } catch { # Handle errors for each mailbox
-        Write-Host "Error retrieving mailboxes: $_" -ForegroundColor Red
+        Write-Host "Error retrieving mailboxes: $_ `n" -ForegroundColor Red
         return
     }
 
@@ -137,36 +169,7 @@ Function mailboxfwd_report {
 
 ###########################
 
-#Call of main function to determine Exchange Online or On-premise and gather domains
+#Call of main function to determine Exchange Online or On-premise, gather domains, and run report
+
 main 
 
-#Add logic to check what report to run
-Write-Host "Ready to run your report `n" -ForegroundColor Green
-# Prompt user for report type
-do {
-    Write-Host "Available reports: publicDL, mailboxfwd, or both" -ForegroundColor Yellow
-    Write-Host "Type 'exit' to quit the script. `n" -ForegroundColor Yellow
-    $reportType = Read-Host "Enter the report type you want to run (publicDL, mailboxfwd, both)"
-    
-    if ([string]::IsNullOrEmpty($reportType)) {
-        Write-Host "Input cannot be empty. Please enter a valid report type." -ForegroundColor Red
-    } elseif ($reportType -notin @("publicDL", "mailboxfwd", "both", "exit")) {
-        Write-Host "Invalid report type. Please enter 'publicDL', 'mailboxfwd', or 'both'." -ForegroundColor Red
-    }
-} while ([string]::IsNullOrEmpty($reportType) -or $reportType -notin @("publicDL", "mailboxfwd", "both", "exit"))
-
-# Run functions based on user input
-if ($reportType -eq "publicDL") {
-    Write-Host "Running public distribution list report..." -ForegroundColor Green
-    publicDL_report
-} elseif ($reportType -eq "mailboxfwd") {
-    Write-Host "Running mailbox forwarding report..." -ForegroundColor Green
-    mailboxfwd_report
-} elseif ($reportType -eq "both") {
-    Write-Host "Running both reports..." -ForegroundColor Green
-    publicDL_report
-    mailboxfwd_report
-} elseif ($reportType -eq "exit") {
-    Write-Host "Exiting script." -ForegroundColor Red
-    break
-}
